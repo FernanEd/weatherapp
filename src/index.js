18ab56e73e3c32f0830875194e2d7c26b3e0e249
FernanEd <fernandoed.bm@gmail.com>
2020-12-31 20:14:11 -0600

    testing animations

--- src/index.js
+++ src/index.js
@@ -11,84 +11,51 @@ if(userCity){
 }
 */
 
-let currentUnit = 0; // 0 - Celsius 1 - Fahrenheit
+let mainScreen = document.querySelector('#main-screen');
+let resultScreen = document.querySelector('#result-screen');
+let cityInput = document.querySelector('#search-input');
+let searchBtn = document.querySelector('#search-btn');
+searchBtn.addEventListener('click', submitHandler);
 
-function parseTemp(temp) {
-  let parsedTemp =
-    currentUnit === 0 ? Number(temp) - 273.1 : 1.8 * (Number(temp) - 273) + 32;
-  return Math.round(parsedTemp * 10) / 10;
-}
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') submitHandler();
+});
 
-async function weatherRequest(city) {
-  try {
-    let response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
-    );
-    let data = await response.json();
-    return data;
-  } catch (e) {
-    console.log(e);
+anime({
+  targets: mainScreen,
+  opacity: [0, 1],
+  translateY: [-20, 0],
+  duration: 3000,
+});
+
+function submitHandler() {
+  if (validateInput()) {
+    // Dissapear the mainscreen on search
+    anime({
+      targets: mainScreen,
+      opacity: [1, 0],
+      duration: 3000,
+    });
+    anime({
+      targets: resultScreen,
+      opacity: [0, 1],
+      translateY: [-20, 0],
+      duration: 3000,
+      begin: () => {
+        mainScreen.style.display = 'none';
+        resultScreen.style.display = 'block';
+      },
+    });
+  } else {
+    anime({
+      targets: cityInput,
+      translateY: [-10, 0],
+    });
   }
 }
 
-weatherRequest('Tampico').then((data) => fillResult(filterData(data)));
-
-function filterData(weatherData) {
-  console.log(weatherData);
-
-  let city = weatherData.name;
-  let country = weatherData.sys.country;
-  let place = `${city}, ${country}`;
-  let { main: weatherTitle, description: weatherDesc } = weatherData.weather[0];
-  let details = weatherData.main;
-  let {
-    feels_like: feeling,
-    humidity,
-    pressure,
-    temp,
-    temp_max: maxTemp,
-    temp_min: minTemp,
-  } = details;
-
-  return {
-    place,
-    weatherTitle,
-    weatherDesc,
-    feeling: parseTemp(feeling),
-    humidity,
-    pressure,
-    temp: parseTemp(temp),
-    maxTemp: parseTemp(maxTemp),
-    minTemp: parseTemp(minTemp),
-  };
-}
-
-function fillResult({
-  place,
-  weatherTitle,
-  weatherDesc,
-  feeling,
-  humidity,
-  pressure,
-  temp,
-  maxTemp,
-  minTemp,
-}) {
-  let results = [
-    { elementId: '#result-temp', value: `${temp}°` },
-    { elementId: '#result-place', value: place },
-    { elementId: '#result-weather', value: weatherTitle },
-    { elementId: '#result-weather-desc', value: weatherDesc },
-    { elementId: '#result-feeling', value: `${feeling}°` },
-    { elementId: '#result-humidity', value: `${humidity}%` },
-    { elementId: '#result-pressure', value: `${pressure}mb` },
-    { elementId: '#result-mintemp', value: `${minTemp}°` },
-    { elementId: '#result-maxtemp', value: `${maxTemp}°` },
-  ];
-
-  for (let { elementId, value } of results) {
-    document.querySelector(elementId).innerText = value;
-  }
+function validateInput() {
+  return cityInput.value !== '';
 }
 
 (() => {
