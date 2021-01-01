5877c94a96e4d913bea0b0aeabbf2ac58a7b86c4
FernanEd <fernandoed.bm@gmail.com>
2020-12-31 19:16:14 -0600

    adding basic filling functions

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import { API_KEY as key } from './apikey';
 
 console.log('yessssss');
 
+let userCity = localStorage.getItem('user_city') || undefined;
+
+/*
+if(userCity){
+    weatherRequest
+}
+*/
+
+let currentUnit = 0; // 0 - Celsius 1 - Fahrenheit
+
+function parseTemp(temp) {
+  let parsedTemp =
+    currentUnit === 0 ? Number(temp) - 273.1 : 1.8 * (Number(temp) - 273) + 32;
+  return Math.round(parsedTemp * 10) / 10;
+}
+
 async function weatherRequest(city) {
   try {
     let response = await fetch(
@@ -15,4 +31,66 @@ async function weatherRequest(city) {
   }
 }
 
-weatherRequest('London').then((data) => console.log(data));
+weatherRequest('Tampico').then((data) => fillResult(filterData(data)));
+
+function filterData(weatherData) {
+  console.log(weatherData);
+
+  let city = weatherData.name;
+  let country = weatherData.sys.country;
+  let place = `${city}, ${country}`;
+  let { main: weatherTitle, description: weatherDesc } = weatherData.weather[0];
+  let details = weatherData.main;
+  let {
+    feels_like: feeling,
+    humidity,
+    pressure,
+    temp,
+    temp_max: maxTemp,
+    temp_min: minTemp,
+  } = details;
+
+  return {
+    place,
+    weatherTitle,
+    weatherDesc,
+    feeling: parseTemp(feeling),
+    humidity,
+    pressure,
+    temp: parseTemp(temp),
+    maxTemp: parseTemp(maxTemp),
+    minTemp: parseTemp(minTemp),
+  };
+}
+
+function fillResult({
+  place,
+  weatherTitle,
+  weatherDesc,
+  feeling,
+  humidity,
+  pressure,
+  temp,
+  maxTemp,
+  minTemp,
+}) {
+  let results = [
+    { elementId: '#result-temp', value: `${temp}°` },
+    { elementId: '#result-place', value: place },
+    { elementId: '#result-weather', value: weatherTitle },
+    { elementId: '#result-weather-desc', value: weatherDesc },
+    { elementId: '#result-feeling', value: `${feeling}°` },
+    { elementId: '#result-humidity', value: `${humidity}%` },
+    { elementId: '#result-pressure', value: `${pressure}mb` },
+    { elementId: '#result-mintemp', value: `${minTemp}°` },
+    { elementId: '#result-maxtemp', value: `${maxTemp}°` },
+  ];
+
+  for (let { elementId, value } of results) {
+    document.querySelector(elementId).innerText = value;
+  }
+}
+
+(() => {
+  const convertBtn = document.querySelector('#convert-units-btn');
+})();
